d5b3983381436cbfc7db6910cd400796126cd33c
Rohit Jha <therohitjha@gmail.com>
2021-08-18 15:44:21 +0530

    Updates

---
+++ precache-manifest.dd2fe4fcc933394ba95466bf5aff2056.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebcc3269c1882e3b8890d6d8b007558",
+    "url": "/react-search-assignment/index.html"
+  },
+  {
+    "revision": "89b4ac648f5e295ed2a5",
+    "url": "/react-search-assignment/static/css/main.704a0eee.chunk.css"
+  },
+  {
+    "revision": "cbcedbb687b38e60690c",
+    "url": "/react-search-assignment/static/js/2.1aae996a.chunk.js"
+  },
+  {
+    "revision": "89b4ac648f5e295ed2a5",
+    "url": "/react-search-assignment/static/js/main.8ec92e21.chunk.js"
+  },
+  {
+    "revision": "92e50a3862a4b5a3b6e0",
+    "url": "/react-search-assignment/static/js/runtime~main.52fd3d3e.js"
+  }
+]);
